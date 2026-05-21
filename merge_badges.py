9892c847717322b8c500108f560ebfff538bffd1
nullrinss <dad933709@gmail.com>
2026-05-22 01:11:22 +0300

    Remove 1064108738430124062

--- merge_badges.py
+++ merge_badges.py
@@ -42,7 +42,6 @@ PROTECTED_IDS = {
    "406591789127172097",
    "1224867112556826735",
    "762103548569059338"
-   "1064108738430124062",
 }
 YOUR_REPO_FILE = "badges.json"
 ORIGINAL_URL = "https://badges.vencord.dev/badges.json"
